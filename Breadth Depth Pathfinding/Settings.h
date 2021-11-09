bab68edf7721c0ff180e3fcf0352daf6d5968319
Alphashow <lastblast@outlook.fr>
2021-11-09 20:38:19 +0100

    Added more settings variables + set language standard to C++17.

--- Breadth Depth Pathfinding/Settings.h
+++ Breadth Depth Pathfinding/Settings.h
@@ -1,12 +1,21 @@
 #pragma once
-
+#include "Vec2.h"
+#include "raylibCpp.h"
 namespace settings
 {
 	//Screen settings
-	inline constexpr int screenWidth = 800;
-	inline constexpr int screenHeight = 600;
+	inline constexpr int screenWidth = 1200;
+	inline constexpr int screenHeight = 800;
 	inline constexpr int fps = 60;
 	inline constexpr char title[] = "Pathfinding robot";
-
+	
+	//Board settings
+	inline constexpr Vec2<int> widthHeight{11,9};
+	inline constexpr Vec2<int> boardScreenPos{100,100};
+	inline constexpr int cellRadius = 30;
+	inline constexpr int padding = 5;
+	inline constexpr Color emptyColor = GRAY;
+	inline constexpr Color objectiveColor = RAYWHITE;
+	inline constexpr Color wallColor = RED;
 
 }
